58e930ba18f4f302dde2900b0531741fa333f2ce
yuki <tokuakeyuki@gmail.com>
2021-02-17 08:02:43 +0000

    Add zoomlike layout

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/zoom', (req, res) => {
+  res.render('zoom');
+});
+
 app.listen(port, () => {
   console.log('listening on *:3000');
 })
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'));
